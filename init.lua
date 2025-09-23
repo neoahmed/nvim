d3fa17f0a22d46e482b0872c8cb92f8f60e4fea6
neoahmed <neoahmed@proton.me>
2025-09-24 01:16:24 +0300

    chore(cleanup): clean init.lua

--- init.lua
+++ init.lua
@@ -1,34 +1,8 @@
 -- nvim/init.lua
--- measure startup time
-vim.g.start_time = vim.fn.reltime()
-
--- faster Lua module loader (Neovim 0.9+)
-if vim.loader then
-  vim.loader.enable()
-end
-
+require("config.options")
 require("config.lazy")
-
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
-
--- override indent for lua files to use 2 spaces
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "lua",
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.expandtab = true
-  end,
-})
-
+require("config.autocmd")
+require("config.mappings")
 ---- load colorscheme
-local theme = require("config.theme")
-theme.load_theme()
-
------------ keymap to switch
-vim.keymap.set("n", "<leader>cs", theme.switch_theme, { desc = "Switch colorscheme" })
-
-
+require("config.theme").load_theme()
 
